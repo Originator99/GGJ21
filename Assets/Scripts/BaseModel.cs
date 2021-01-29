39596026321711d0ec9a477b476b4f9ed4f60648
Originator99 <arsalaan.md0077@gmail.com>
2021-01-29 20:46:21 +0530

    first

---
+++ Assets/Scripts/BaseModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseModel : MonoBehaviour {
+    public Transform rig;
+    public Collider[] colliders;
+    public Animator animator;
+    public Collider mainCollider;
+    public Rigidbody rb;
+    public Transform explosionPos;
+
+    private void Start() {
+        colliders = rig.GetComponentsInChildren<Collider>();
+        DoRagDoll(false);
+    }
+
+    public void DoRagDoll(bool isRagdoll) {
+        foreach(var collider in colliders) {
+            collider.enabled = isRagdoll;
+        }
+        animator.enabled = !isRagdoll;
+        mainCollider.enabled = !isRagdoll;
+        rb.useGravity = !isRagdoll;
+    }
+
+    public void AddForce() {
+        rb.AddExplosionForce(100, explosionPos.position, 5f, 3f);
+    }
+}
---
+++ Assets/Scripts/PointForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointForce : MonoBehaviour {
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            AddForce(Input.mousePosition);
+        }
+    }
+
+    private void AddForce(Vector3 mousePos) {
+        var ray = Camera.main.ScreenPointToRay(mousePos);
+        var hits = Physics.RaycastAll(ray);
+        foreach(var hit in hits) {
+            var enemy = hit.collider.GetComponent<BaseModel>();
+            if(enemy != null) {
+                enemy.AddForce();
+                enemy.DoRagDoll(true);
+            }
+        }
+    }
+}
