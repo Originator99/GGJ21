dab8015496131466288209a5aa098c82def87cc0
ap4996 <a.prabuddha@gmail.com>
2021-01-31 01:57:34 +0530

    portal-scene

---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+    public PortalNumber portalNumber;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("player entered portal " + portalNumber);
+            Debug.Log("Level" + portalNumber);
+            Debug.Log("Level" + (int)portalNumber);
+            Debug.Log("Level" + ((int)portalNumber));
+            LoadLevelScene();
+        }
+    }
+
+    private void LoadLevelScene()
+    {
+        SceneManager.LoadScene("Level" + (int)portalNumber);
+    }
+}
+
+public enum PortalNumber
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five
+}
