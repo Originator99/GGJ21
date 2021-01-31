ac2d8785c0c204cda9e900510764259a07848533
ap4996 <a.prabuddha@gmail.com>
2021-01-31 10:45:31 +0530

    platform-prefab

---
+++ Assets/Scripts/EnemyBrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBrain : MonoBehaviour
+{
+    public Transform LookAt;
+    public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LookAt != null)
+        {
+            transform.LookAt(LookAt);
+            if(Vector3.Distance(LookAt.position, transform.position) > 2f)
+                transform.position = Vector3.MoveTowards(transform.position, LookAt.position, movementSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public EnemySpawnArea spawnArea;
+
+    public GameObject enemyObject, playerObject;
+
+    private float xPos, yPos, zPos;
+
+    private void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    private IEnumerator EnemyDrop()
+    {
+        for(int i = 0; i < spawnArea.enemyCount; ++i)
+        {
+            xPos = Random.Range(spawnArea.minPos.x, spawnArea.maxPos.x);
+            yPos = Random.Range(spawnArea.minPos.y, spawnArea.maxPos.y);
+            zPos = Random.Range(spawnArea.minPos.z, spawnArea.maxPos.z);
+            Instantiate(enemyObject, new Vector3(xPos, yPos, zPos), Quaternion.identity).GetComponent<EnemyBrain>().LookAt = playerObject.transform;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
+
+[System.Serializable]
+public struct EnemySpawnArea
+{
+    public Vector3 minPos, maxPos;
+    public int enemyCount;
+}
